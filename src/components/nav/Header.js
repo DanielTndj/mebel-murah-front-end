b0033d97a15e3bfe03499d92943e61e818026d75
DanielTndj <daniel.tndj@gmail.com>
2021-01-11 20:48:20 +0700

    Change dynamically menu base on user role

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import {
-  AppstoreOutlined,
+  BuildOutlined,
+  HomeOutlined,
   LogoutOutlined,
   UserAddOutlined,
   UserOutlined,
@@ -37,7 +38,7 @@ const Header = () => {
 
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Item key="home" icon={<AppstoreOutlined />}>
+      <Item key="home" icon={<BuildOutlined />}>
         <Link to="/">Home</Link>
       </Item>
 
@@ -60,8 +61,17 @@ const Header = () => {
           title={user.email && user.email.split("@")[0]}
           className="float-right"
         >
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+          {user && user.role === "customer" && (
+            <Item icon={<HomeOutlined />}>
+              <Link to="/user/history">Dashboard</Link>
+            </Item>
+          )}
+
+          {user && user.role === "admin" && (
+            <Item icon={<HomeOutlined />}>
+              <Link to="/admin/dashboard">Dashboard</Link>
+            </Item>
+          )}
           <Item icon={<LogoutOutlined />} onClick={logout}>
             Logout
           </Item>
