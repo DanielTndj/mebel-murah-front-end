484cd78227d3dfc15a5f46217f1e5d6a2c389b1d
DanielTndj <daniel.tndj@gmail.com>
2021-01-11 23:57:09 +0700

    Create search component

---
+++ src/components/forms/LocalSearch.js
@@ -0,0 +1,28 @@
+import { Input } from "antd";
+import React from "react";
+
+const LocalSearch = ({ keyword, setKeyword }) => {
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+
+    setKeyword(value.toLowerCase());
+  };
+
+  return (
+    <div className="row pb-2">
+      <div className="col-md-4">
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="text-secondary"
+          style={{ borderRadius: "150px" }}
+          value={keyword}
+          onChange={handleSearch}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LocalSearch;
